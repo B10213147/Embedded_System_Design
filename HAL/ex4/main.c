9b60cc41a200e55b48fee0c5e361a57783cd82b9
B10213147 <B10213147@yuntech.org.tw>
2016-08-02 17:19:56 +0800

    Finished EXTI0 handler

--- HAL/ex4/main.c
+++ HAL/ex4/main.c
@@ -1,9 +1,21 @@
 #include "stm32f0xx.h"                  // Device header
-#include "stm32f0xx_hal_gpio.h"
-#include "stm32f0xx_hal_rcc.h"
 
+void GPIOC_Config(void);
+void EXTI0_1_IRQHandler_Config(void);
+void TIM2_IRQHandler_Config(void);
+
+int led_f = 1;	// LED frequency
 int main(void){
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+	/* Condigure GPIOC*/
+	GPIOC_Config();
+	
+	/* Configure EXTI_Line0 (connected to PA.00 pin) in interrupt mode */
+	EXTI0_1_IRQHandler_Config();
+	
+	while(1);
+}
+
+void GPIOC_Config(void){
 	__HAL_RCC_GPIOC_CLK_ENABLE();
 	
 	GPIO_InitTypeDef GPIO_Init;
@@ -12,18 +24,46 @@ int main(void){
 	GPIO_Init.Pull = GPIO_NOPULL;
 	GPIO_Init.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOC, &GPIO_Init);
+}
+
+void EXTI0_1_IRQHandler_Config(void){
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 	
+	GPIO_InitTypeDef GPIO_Init;
 	GPIO_Init.Pin = GPIO_PIN_0;
-	GPIO_Init.Mode = GPIO_MODE_INPUT;
+	GPIO_Init.Mode = GPIO_MODE_IT_RISING;
+	GPIO_Init.Pull = GPIO_NOPULL;
+	GPIO_Init.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOA, &GPIO_Init);
 	
-	while(1){
-		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) != 0){
-		while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) != 0){			
-			HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
-			for(int i=0; i<80000; i++);
-		}
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
-		}
+	HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
+
+void TIM2_IRQHandler_Config(void){
+	__HAL_RCC_TIM2_CLK_ENABLE();
+	
+	TIM_Base_InitTypeDef Structure;
+	Structure.ClockDivision = 0;
+	Structure.CounterMode = TIM_COUNTERMODE_UP;
+	Structure.Period = 0xffffffff;
+	Structure.Prescaler = TIM_CLOCKDIVISION_DIV1;	
+	TIM_Base_SetConfig(TIM2, &Structure);
+	
+	TIM_HandleTypeDef htim;
+	htim.Instance = TIM2;
+
+	
+	HAL_TIM_OC_Start_IT(&htim, TIM_CHANNEL_1);
+	
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	if(GPIO_Pin == GPIO_PIN_0){
+		led_f = 1 + (led_f % 10);
 	}
+}
+
+void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim){
+	
 }
---
+++ HAL/ex4/stm32f0xx_it.c
@@ -0,0 +1,83 @@
+#include "stm32f0xx_it.h"
+/******************************************************************************/
+/*            Cortex-M0 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+/******************************************************************************/
+/*                 STM32F0xx Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f0xx.s).                                               */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles external line 0 interrupt request.
+  * @param  None
+  * @retval None
+  */
+void EXTI0_1_IRQHandler(void)
+{
+	if(EXTI->PR & EXTI_PR_PIF0){
+		HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+	}
+}
+
+void TIM2_IRQHandler(void)
+{
+	
+}
+/**
+  * @}
+  */
